bbad53c982043bdb1a31a265147b25dcb8df16a9
Sobhit jadoun <65443674+Hackertrackersj@users.noreply.github.com>
2020-10-01 16:07:21 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,22 +5,22 @@ cd
 echo "\033[1;93m#NITRO HACKER"
 figlet  "NITRO Hacker"
 echo ""
-apt update
-apt upgrade
+apt update -y 
+apt upgrade -y
 echo "$(tput setaf 5) Subscribe < Nitro Hacker YT channel For more Termux videos "
 echo ""
 echo "$(tput setaf 10) Installing FIGLET......."
-apt install figlet
+apt install figlet -y
 echo "$(tput setaf 6) Installing TOILET......."
-apt install toilet
+apt install toilet -y
 echo ""
 echo "$(tput setaf 10) Installing COWSAY......."
 figlet -f Bubble "installing cowsay"
-apt install cowsay
+apt install cowsay -y
 echo ""
 echo "$(tput setaf 14) Installing NODE-JS......."
 figlet -f Bubble "installing NodeJs"
-apt install nodejs
+apt install nodejs -y
 echo ""
 echo "$(tput setaf 5) Installing FIGLET-cli......."
 npm install figlet
@@ -36,7 +36,7 @@ figlet -f small "Done!"
 
 echo "$(tput setaf 10) installation Succesful [DONE!]"
 echo "|-----------------------------|"
-echo "$(tput setaf 3) Nitro Hacker.."
+echo "$(tput setaf 3) Subscribe yt channelNitro Hacker.."
 echo "|-----------------------------|"
 echo ""
 echo "Now Run This Tool."
